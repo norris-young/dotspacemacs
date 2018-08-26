ec3e7434ec04596e81107adb923cdb5020990de5
Norris Young <shading.young@gmail.com>
2018-08-26 23:40:07 +0800

    NEW: maximize window at startup.
    Signed-off-by: Norris Young <shading.young@gmail.com>

--- init.el
+++ init.el
@@ -312,6 +312,7 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
+  (add-to-list 'default-frame-alist '(fullscreen . maximized))
   (setq
    ;; Use emacs-china mirrors for elap archives.
    configuration-layer--elpa-archives '(("melpa-cn" . "https://elpa.emacs-china.org/melpa/")
